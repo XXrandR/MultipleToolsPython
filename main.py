fa9df97bf20f05e39dc9f5bf663e36a0cd73e665
XXrandR <xxrandr@gmail.com>
2023-04-14 16:07:08 -0500

    change something

--- main.py
+++ main.py
@@ -1,35 +1,17 @@
-import pandas as pd
-import os
 import PyPDF2
 
-#Create empty txt files Named as pdf files 
+path = '/home/cisco/Downloads/Leader.pdf'
+newpath = '/home/cisco/result.txt'
 
-path = '~/Downloads/'
-newpath = '~/'
+reader = PyPDF2.PdfReader(path)
+pag = 0
 
-# r=root, d=directories, f = files
-
-files = []
-for r, d, f in os.walk(path):
-    for file in f:
-        if '.pdf' in file:
-            files.append(os.path.join(r, file))
-
-for f in files:
-
-    txt = f.replace('.pdf','.txt')
-    txtpath = txt.replace(path,newpath) 
-    print(f)
-    ft= open(txtpath ,"w+")
-    ft.close()
-    print(txtpath)
-    Vpath = f.replace('.pdf','')
-    # print(Vpath)
-
-    myPDFFile = PyPDF2.PdfFileReader(f)
-    with open(txtpath, 'w') as pdf_output:   #, encoding="utf-8"
-         for page in range (myPDFFile.getNumPages()):
-            data = myPDFFile.getPage(page).extractText()
-            pdf_output.write(data)            
-    with open(txtpath, 'r') as myPDFContent:
-        myPDFContent.read().replace('\n',' ')
+f = open(newpath, "w")
+while pag < len(reader.pages):
+    page = reader.pages[pag]
+    pag+=1;
+    print("Page: ", pag)
+    a = page.extract_text()
+    print(a)
+    f.write(a)
+f.close()
